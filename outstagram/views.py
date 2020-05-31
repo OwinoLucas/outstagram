57b0547ec6fd6dd030a6b0ecba9930c8974e633f
Lucas Owino <lucowish35@gmail.com>
2020-05-31 14:39:15 +0300

    created .env file and stored it inside the .gitignore file to ensure critical info is not pushed to remote repo

--- outstagram/views.py
+++ outstagram/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+
 
 # Create your views here.
 def index(request):
+    """
+    view function renders the landing page
+    """
     return render(request, 'index.html')
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/lucas/Documents/outstagram/venv/bin/python
+#!/home/lucas/Documents/OutstaGram/venv/bin/python
 from django.core import management
 
 if __name__ == "__main__":
