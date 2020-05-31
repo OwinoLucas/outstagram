270b2101d3a80589256f373ce7798d497efb982d
Lucas Owino <lucowish35@gmail.com>
2020-05-31 15:15:28 +0300

    dropped user table since django has an inbuilt user auth model

--- outstagram/models.py
+++ outstagram/models.py
@@ -1,10 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    """
-    class containing user objects
-    """
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=140)
-    password = models.CharField(max_length=100)
--- outstagram/tests.py
+++ outstagram/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import User
+
 
 # Create your tests here.
+class UserTestClass(TestCase):
+    """
+    test class to test user objects
+    """
+    def setUp(self):
+        self.lucas = User(username = 'Lucas',email = 'luca@gmail.com',password = 'qwerty')
