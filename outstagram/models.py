9b2394cbf84676e92deaf3fb83b6eb024c47805f
Lucas Owino <lucowish35@gmail.com>
2020-06-01 17:39:55 +0300

    overide the image save method and used PIL to check and resize large profile pic images

--- outstagram/models.py
+++ outstagram/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from  django.contrib.auth.models import User
-
+from PIL import Image
 
 # Create your models here.
 class Profile(models.Model):
@@ -20,7 +20,13 @@ class Profile(models.Model):
         """
         method saves entered profiles to the database
         """
-        self.save()
+        super().save()
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
     def update_profile(self, using=None, fields=None, **kwargs):
         """
